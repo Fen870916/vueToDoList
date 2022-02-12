6faf89374ee13136d76d39a1a70f27da2362992a
fen <a0903169366@gmail.com>
2022-02-13 02:58:14 +0800

    完成新增功能

--- js/index.js
+++ js/index.js
@@ -4,5 +4,21 @@ let data = {
 
 let vm = new Vue({
   el: '#app',
-  data: data
+  data: data,
+  methods: {
+    clickAdd: function () {
+      if (document.querySelector('.enter').value.trim().length !== 0) {
+        let matter = document.createElement('div')
+        matter.classList.add('matter')
+        matter.innerHTML =
+          `<button class="status undone"><i class="fas fa-times"></i></button>
+      <div class="content">` +
+          document.querySelector('.enter').value +
+          `</div>
+          <button class="delete"><i class="far fa-trash-alt"></i></button>`
+        document.querySelector('.list').appendChild(matter)
+        document.querySelector('.enter').value = ''
+      }
+    }
+  }
 })
